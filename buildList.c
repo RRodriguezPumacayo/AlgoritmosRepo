e7b4e3307d992e469546b5ffbb847bffd4c451b8
Ricardo <rrodriguezp@unsa.edu.pe>
2023-12-05 18:22:41 -0500

    first commit

---
+++ buildList.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Primero declaramos la estructura nodo
+struct node {
+	int data;
+	struct node* next;
+};
+
+// Declaramos la funcion BuildOneTwoThree de tipo node*
+struct node* BuildOneTwoThree()
+{
+	struct node* head = NULL;
+	struct node* second = NULL;
+	struct node* third = NULL;
+
+	head = malloc(sizeof(struct node));
+	second = malloc(sizeof(struct node));
+	third = malloc(sizeof(struct node));
+
+	head->data = 1;
+	head->next = second;
+
+	second->data = 2;
+	second->next = third;
+
+	third->data = 3;
+	third->next = NULL;
+
+	return head;
+}
+
+// Imprimir
+void print(struct node* head)
+{
+	while(head){
+	  printf("%d ", head->data);
+	  head = head->next;
+	}
+	printf("\n");
+}
+
+int main()
+{
+	struct node* lista;
+	lista = BuildOneTwoThree();
+	print(lista);
+	return 0;
+}	
